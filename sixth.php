65e042391bce3a3c327710a2953f5b9534e27c31
Rakhat Zhumekin <190103441@stu.sdu.edu.kz>
2021-02-03 17:46:07 +0600

    My sixth commit

---
+++ sixth.php
@@ -0,0 +1,1 @@
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quam possimus asperiores quidem magnam assumenda temporibus, iure expedita aspernatur cum omnis nulla magni inventore? Magni est amet eum accusamus quis?
